a6a9895d9b4c7d15f7091da76d23c387b0d8000e
Jose Sanchez-Capo <josesanchezcapo@me.com>
2021-01-15 21:31:44 -0600

    Update file

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,34 @@
-var connection = require("./connection.js");
-
-// Object Relational Mapper (ORM)
+var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function (tableInput, valOfCol) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            valOfCol(result);
-        });
-    },
-    insertOne: function (tableInput, col, valOfCol) {
-        var queryString = "INSERT INTO " + tableInput + "(" + col + ") VALUES (" + valOfCol + ");";
-
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            valOfCol(result);
-        });
-    },
-    updateOne: function (tableInput, col, condition, valOfCol) {
-        var queryString = "UPDATE " + tableInput + " SET " + col + " WHERE " + condition;
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            valOfCol(result);
-        });
-    }
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, value, cb) {
+    var queryString = "INSERT INTO " + table + " SET ?";
+    connection.query(queryString, value, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  
+  updateOne: function(table, condition, id, cb) {
+    var queryString = "UPDATE " + table + " SET " + condition + " WHERE id = ?";
+    connection.query(queryString, id, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
